822ffa0a083b0eaa4915c7b3b36831e462d9ae4f
Frédéric Bour <frederic.bour@lakaban.net>
2015-02-17 10:10:00 +0100

    Refactor proof

--- proof/bt.v
+++ proof/bt.v
@@ -23,52 +23,48 @@ Fixpoint depth {n} f (l : lbst n) : nat :=
   | Node _ _ _ l r => S (f (depth f l) (depth f r))
   end.
 
+Theorem bounded_step n n' d total 
+  (Hn : n <= n')
+  (Htotal : n' * 5 < total * 4)
+  (Hd : 5 ^ d <= 4 ^ d * n)
+  : 5 ^ (S d) <= 4 ^ (S d) * total.
+Proof.
+  do 2 rewrite (pow_succ_r _ _ (le_0_n _)).
+  rewrite <- mult_assoc.
+
+  (* <= weakens ?! *)
+  assert ((4 ^ d) * (n' * 5) <= (4 ^ d) * (total * 4)).
+  apply mult_le_compat_l.
+  intuition.
+
+  rewrite <- Nat.mul_shuffle3.
+  apply (le_trans _ (4 ^ d * (n' * 5)) _); intuition.
+  
+  rewrite (mult_comm n').
+  rewrite Nat.mul_shuffle3.
+  apply mult_le_compat_l.
+
+  apply (le_trans _ (4 ^ d * n) _).
+  trivial.
+  
+  apply mult_le_compat_l; auto.
+Qed.
+
 Theorem bounded_max {n} (l : lbst n) : 5 ^ (depth max l) <= 4 ^ (depth max l) * n.
 Proof.
   remember l.
   induction l.
   subst; compute; intuition.
 
-  assert (Hl1 := IHl1 l1 eq_refl).
-  assert (Hl2 := IHl2 l2 eq_refl).
-  clear IHl1 IHl2.
-
   assert ((max a b) * 5 < (1 + a + b) * 4).
   apply (bounded_disbalance a b l0 b0 l1 l2 Heql0).
 
-  assert (depth max l0 = S (max (depth max l1) (depth max l2))).
-  rewrite Heql0. reflexivity.
-
+  rewrite Heql0.
+  simpl (depth max _).
+ 
   destruct (lt_dec (depth max l1) (depth max l2)).
-  * rewrite max_r in H0; intuition.
-    rewrite H0.
-    
-    assert (5 * 5 ^ depth max l2 <= 4 * 4 ^ depth max l2 * (1 + a + b)).
-    apply (le_trans _ (5 * (4 ^ depth max l2 * b)) _).
-    apply mult_le_compat_l. intuition.
-    rewrite mult_assoc.
-    
-    assert (5 * b <= 4 * (1 + a + b)).
-    assert (b <= max a b). intuition.
-    apply (le_trans _ (5 * max a b) _); intuition.
-    rewrite Nat.mul_shuffle0.
-    rewrite (Nat.mul_shuffle0 4).
-    apply mult_le_compat; auto.
-    exact H1.
-    
-  * rewrite max_l in H0; intuition.
-    rewrite H0.
-    
-    assert (5 * 5 ^ depth max l1 <= 4 * 4 ^ depth max l1 * (1 + a + b)).
-    apply (le_trans _ (5 * (4 ^ depth max l1 * a)) _).
-    apply mult_le_compat_l. intuition.
-    rewrite mult_assoc.
-    
-    assert (5 * a <= 4 * (1 + a + b)).
-    assert (a <= max a b). intuition.
-    apply (le_trans _ (5 * max a b) _); intuition.
-    rewrite Nat.mul_shuffle0.
-    rewrite (Nat.mul_shuffle0 4).
-    apply mult_le_compat; auto.
-    exact H1.
+  * rewrite max_r; intuition.
+    apply (bounded_step b (max a b)); intuition.
+  * rewrite max_l; intuition.
+    apply (bounded_step a (max a b)); intuition.
 Qed.
